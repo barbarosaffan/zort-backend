28638c61b527fa5f21799f80d9481786907fc6b7
barbaros affan yildirim <barbarosayildirim@gmail.com>
2024-03-05 01:18:22 +0300

    feat: add gin & ping server

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	err := r.Run()
+
+	if err != nil {
+		log.Fatalf("r.Run() error: %s", err)
+	}
+}
